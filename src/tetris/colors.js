36eddb23e02ea09011c2969b44365c2fafe9f71e
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-03-04 19:00:35 +0530

    colors

---
+++ src/tetris/colors.js
@@ -0,0 +1,20 @@
+export const colors = [
+  "tomato",
+  "#537C8E",
+  "#F2E436",
+  "#EFAA2B",
+  "#F17427",
+  "#00B0B9",
+  "#FFC300",
+  "#3FC380",
+  "#2ECC71",
+  "#3498DB",
+  "#3498DB",
+  "#2ECC71",
+  "#40E0D0",
+  "#DFFF00",
+  "#CCCCFF",
+  "#6495ED",
+  "#DE3163",
+  "#9FE2BF"
+];
